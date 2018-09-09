57ecfe11882540027fecb17f24a7e24b85688db9
Jiaying-Liu <jiaying_liu1996@hotmail.com>
2018-09-08 23:47:51 -0400

    fixed axios requests

--- docs/src/actions/index.js
+++ docs/src/actions/index.js
@@ -4,29 +4,31 @@ import {
     LOGIN
 } from './types';
 
+var defaultAxios = axios;
+
 if(process.env.REACT_APP_API_URL) {
-    axios = axios.create({
+    defaultAxios = axios.create({
         baseURL: process.env.REACT_APP_API_URL
     });  
 }
 
 // session actions
 export const login = (username, password) => async dispatch => {
-    const res = await axios.post('/auth', {username, password});
+    const res = await defaultAxios.post('/auth', {username, password});
 
     dispatch({ type: LOGIN, payload: res.data });
 } 
 
 export const register = (name, username, password) => async dispatch => {
-    const res = await axios.post('/api/register', {name, username, password});
+    const res = await defaultAxios.post('/api/register', {name, username, password});
 
     dispatch({ type: LOGIN, payload: res.data });
 }
 
 // user actions
 export const fetchUser = () => async (dispatch, getState) => {
-    axios.defaults.headers.common['Authorization'] = 'JWT ' + sessionStorage.access_token;
-    const res = await axios.get('/api/current_user');
+    defaultAxios.defaults.headers.common['Authorization'] = 'JWT ' + sessionStorage.access_token;
+    const res = await defaultAxios.get('/api/current_user');
 
     dispatch({ type: FETCH_USER, payload: res.data });
 }
